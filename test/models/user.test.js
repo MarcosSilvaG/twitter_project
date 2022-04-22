9523a72a51d0f4b31f29cf85be34c8980932ac1c
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-04-21 21:31:51 -0500

    Refactoring user.js and user.test.js

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -3,13 +3,17 @@ const User = require("./../../app/models/user")
 describe("Unit Test for User class", () => {
     test("1) Create a user object", () => {
 
-        const user = new User(1, "Washiprah", "Marcos Silva", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "Washiprah", "Marcos Silva", "Bio")
 
         expect(user.id).toBe(1)
         expect(user.username).toBe("Washiprah")
         expect(user.name).toBe("Marcos Silva")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
+    }),
+    test("", () => {
+        
     }) 
 }) 
