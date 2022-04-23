f148698a39557b7884cc69695ae7ad5052451106
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-04-22 20:31:44 -0500

    Adding test and get method to  UserServices.js file

--- test/services/UserServices.test.js
+++ test/services/UserServices.test.js
@@ -9,4 +9,13 @@ describe('Test for UserService', () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     })
+    test("2) Get all user data in a List", () => {
+        const User = UserService.create(1, "washiprah", "Marcos Silva")
+        const userInfoInList = UserService.getInfo(User)
+        
+        expect(userInfoInList[0]).toBe(1)   
+        expect(userInfoInList[1]).toBe("washiprah")
+        expect(userInfoInList[2]).toBe("Marcos Silva")
+        expect(userInfoInList[3]).toBe("Sin bio")
+    })
 })  
