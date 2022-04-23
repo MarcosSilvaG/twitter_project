01b0b0a8f7816ea520ea753d6a98b90012926e99
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-04-22 21:13:59 -0500

    Adding test and updateUserUserName method to Userservices.js

--- test/services/UserServices.test.js
+++ test/services/UserServices.test.js
@@ -10,12 +10,17 @@ describe('Test for UserService', () => {
         expect(user.bio).not.toBeUndefined()
     })
     test("2) Get all user data in a List", () => {
-        const User = UserService.create(1, "washiprah", "Marcos Silva")
-        const userInfoInList = UserService.getInfo(User)
+        const user = UserService.create(1, "washiprah", "Marcos Silva")
+        const userInfoInList = UserService.getInfo(user)
         
         expect(userInfoInList[0]).toBe(1)   
         expect(userInfoInList[1]).toBe("washiprah")
         expect(userInfoInList[2]).toBe("Marcos Silva")
         expect(userInfoInList[3]).toBe("Sin bio")
+    })
+    test("3) Update username", () => {
+        const user = UserService.create(1, "washiprah", "Marcos Silva")
+        UserService.updateUserUserName(user, "washisaurio")
+        expect(user.username).toBe("washisaurio")    
     })
 })  
